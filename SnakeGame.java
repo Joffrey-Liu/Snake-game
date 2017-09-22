54f19fc86726eee331d0f173591a6213cbee130e
Joffrey-Liu <32201219+Joffrey-Liu@users.noreply.github.com>
2017-09-22 23:33:20 +0800

    Java version

---
+++ SnakeGame.java
@@ -0,0 +1,208 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SnakeGame extends JFrame {
+	public static final int WIDTH = 800, HEIGHT = 600, SLEEPTIME = 100, L = 1,R = 2, U = 3, D = 4;
+	BufferedImage offersetImage= new BufferedImage(WIDTH, HEIGHT,BufferedImage.TYPE_3BYTE_BGR);;
+	Rectangle rect = new Rectangle(20, 40, 15 * 50, 15 * 35);//background
+	Snake snake;
+	SNode node;
+	Boolean stop = false;//
+	Container c;
+	JLabel lab1;
+	JMenuBar bar;
+	Boolean replay = false;
+	public SnakeGame() {
+		snake = new Snake(this);
+		createNode();
+		this.setBounds(0, 100, WIDTH, HEIGHT);//(Y)
+		this.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent arg0) {
+			
+			
+				switch (arg0.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					snake.dir = L;
+					break;
+				case KeyEvent.VK_RIGHT:
+					snake.dir = R;
+					break;
+				case KeyEvent.VK_UP:
+					snake.dir = U;
+					break;
+				case KeyEvent.VK_DOWN:
+					snake.dir = D;
+					break;
+				case KeyEvent.VK_R:
+						stop = true;
+						break;
+				case KeyEvent.VK_Z:	
+						replay = true;
+						
+				case KeyEvent.VK_S:
+						stop = false;
+			
+				
+			}
+			}
+		});
+		/*c=getContentPane();//ContentPane
+		//
+		c.setLayout(new FlowLayout());
+		bar = new JMenuBar();
+		JMenu file = new JMenu("File(F)");
+		file.setMnemonic(KeyEvent.VK_F);*/
+		
+		
+		this.setTitle("Snake Game 0.1   By ");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		new Thread(new ThreadUpadte()).start();//timer/////////////////////
+	}
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) offersetImage.getGraphics();
+		g2d.setColor(Color.white);//
+		g2d.fillRect(0, 0, WIDTH, HEIGHT);
+		//g.drawImage(image,0,0,WIDTH,HEIGHT,this);
+		g2d.setColor(Color.black);//
+		g2d.drawRect(rect.x, rect.y, rect.width, rect.height);
+		if (snake.hit(node)) {
+			createNode();
+		}
+		snake.draw(g2d);
+		node.draw(g2d);
+		g.drawImage(offersetImage, 0, 0, null);
+	}
+	class ThreadUpadte implements Runnable {
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(SLEEPTIME);
+					if(stop == false)
+						repaint();
+					if(replay == true)
+						repaint();
+				} catch (InterruptedException e) {
+					//e.printStackTrace();
+				}
+			}
+		}
+	}
+	public void createNode() {//new food
+		int x = (int) (Math.random() * 650) + 50,y = (int) (Math.random() * 500) + 50;
+		node = new SNode(x, y, Color.blue);
+	}
+	public static void main(String args[]) {
+		new SnakeGame();
+	}
+}
+class SNode {//node 
+	int x, y, width = 15, height = 15;
+	Rectangle rect = new Rectangle(x, y, width, height);
+	Color color;
+	public SNode(int x, int y, Color color) {
+		this.x = x; this.y = y;
+		this.color = color;
+	}
+	public void draw(Graphics2D g2d) {
+		g2d.setColor(color);
+		g2d.fillOval(x, y, width, height);
+	}
+	public Rectangle getRect() {
+		rect.x = x; rect.y = y;
+		return rect;
+	}
+}
+class Snake {   
+	public List<SNode> nodes = new ArrayList<SNode>();
+	SnakeGame interFace;
+	int dir=SnakeGame.R;//
+	public Snake(SnakeGame interFace) {
+		this.interFace = interFace;
+		nodes.add(new SNode(20 + 150, 40 + 150, Color.black));//initial location
+		addNode();
+	}
+	public boolean hit(SNode node) {
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes.get(i).getRect().intersects(node.getRect())) {
+				addNode();
+				return true;
+			}
+		}
+		return false;
+	}
+	public void draw(Graphics2D g2d) {
+		for (int i = 0; i < nodes.size(); i++) {
+			nodes.get(i).draw(g2d);
+			//interFace.replay = false;
+		}
+		if(interFace.replay == true)
+		{
+				nodes.clear();
+				nodes.add(new SNode(20 + 150, 40 + 150, Color.black));
+				addNode();
+				dir = SnakeGame.R;
+				interFace.replay = false;
+		}
+		move();
+	}
+	public void move() {
+		nodes.remove((nodes.size() - 1));
+		addNode();
+	}
+	public synchronized void addNode() {
+		SNode nodeTempNode = nodes.get(0);
+		
+		switch (dir) {
+		case SnakeGame.L:
+			if (nodeTempNode.x <= 20) {
+				//nodeTempNode = new SNode(20 + 15 * 50, nodeTempNode.y, Color.black);
+				
+					interFace.stop = true;
+				
+				
+				
+			}
+			nodes.add(0, new SNode(nodeTempNode.x - nodeTempNode.width, nodeTempNode.y, Color.pink ));
+			break;
+		case SnakeGame.R:
+			if (nodeTempNode.x >= 20 + 15 * 50 - nodeTempNode.width) {
+				//nodeTempNode = new SNode(20 - nodeTempNode.width, nodeTempNode.y, Color.black);
+				
+					interFace.stop = true;
+				
+		}
+			nodes.add(0, new SNode(nodeTempNode.x + nodeTempNode.width, nodeTempNode.y, Color.gray));
+			break;
+		case SnakeGame.U:
+			if (nodeTempNode.y <= 40) {
+				//nodeTempNode = new SNode(nodeTempNode.x, 40 + 15 * 35, Color.black);
+				
+					interFace.stop = true;
+				
+			}
+			nodes.add(0, new SNode(nodeTempNode.x, nodeTempNode.y - nodeTempNode.height, Color.blue));
+			break;
+		case SnakeGame.D:
+			if (nodeTempNode.y >= 40 + 15 * 35 - nodeTempNode.height) {
+				//nodeTempNode = new SNode(nodeTempNode.x,40 - nodeTempNode.height, Color.black);
+				
+					interFace.stop = true;
+				
+			}
+			nodes.add(0, new SNode(nodeTempNode.x, nodeTempNode.y + nodeTempNode.height, Color.red));
+			break;
+		}
+	}
+}
